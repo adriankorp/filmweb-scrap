1c2ee35c792897e833571d18ac8894af854226cf
adriankorp <adrian.korpowski@gmail.com>
2023-07-08 09:13:16 +0200

    Functions to scrap 10 top movies with rating

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import axios from 'axios';
 import * as cheerio from 'cheerio';
 
-const URL = 'https://www.filmweb.pl/ranking/vod/film';
+const URL_RANKING = 'https://www.filmweb.pl/ranking/vod/film';
+const URL = 'https://www.filmweb.pl';
 
 const getHtml = async (url: string) => {
     const { data } = await axios.get(url);
     return data;
 };
 
-const getTopFourProviders = async ($: cheerio.CheerioAPI) => {
+const getTopFourProviders = async () => {
+    const html = await getHtml(URL_RANKING);
+    const $ = cheerio.load(html);
     const providerList = $('.rankingProvider__list');
     const providersLi = providerList.find('li');
     const topFourProviders = providersLi.slice(0, 4);
@@ -28,10 +31,58 @@ const getTopFourProviders = async ($: cheerio.CheerioAPI) => {
     console.log(providers);
     return providers;
 };
-const main = async () => {
-    const html = await getHtml(URL);
+
+const getMovieTitle = ($: cheerio.CheerioAPI, movie: cheerio.Element) => {
+    const title = $(movie).find('.rankingType__title').find('a').text();
+    return title;
+};
+
+const getMovieRating = ($: cheerio.CheerioAPI, movie: cheerio.Element) => {
+    const rating = $(movie).find('.rankingType__rate--value').text();
+    return rating;
+};
+
+const getTopTenMovies = async (url: string | undefined, year: string) => {
+    if (!url) {
+        return;
+    }
+    const html = await getHtml(`${URL}${url}/${year}`);
     const $ = cheerio.load(html);
-    getTopFourProviders($);
+    const moviesList = $('.rankingTypeSection__container');
+    const movies = moviesList.find('.rankingType.hasVod');
+    const topTenMovies = movies.slice(0, 10);
+
+    const moviesData = topTenMovies
+        .map((i, el) => {
+            const title = getMovieTitle($, el);
+            const rating = getMovieRating($, el);
+            const movie = {
+                title,
+                rating,
+            };
+            return movie;
+        })
+        .get();
+
+    console.log(moviesData);
+};
+
+const getActualYear = () => {
+    const date = new Date();
+    const year = date.getFullYear();
+    return year.toString();
+};
+
+const main = async () => {
+    const providers = await getTopFourProviders();
+    const actualYear = getActualYear();
+
+    const moviesData = Promise.all(
+        providers.map(async (provider) => {
+            await getTopTenMovies(provider.providerUrl, actualYear);
+        }),
+    );
+    console.log(moviesData);
 };
 
 main();
